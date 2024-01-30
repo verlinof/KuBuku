29482b6caaa06d1040121be19eabe0336da3ee14
verlinof <ajikoko.zx@gmail.com>
2024-01-30 11:09:42 +0700

    Feat: DashboardActivity, HomeFragment

---
+++ app/src/main/java/com/example/kubuku/fragment/LibraryFragment.kt
@@ -0,0 +1,23 @@
+package com.example.kubuku.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kubuku.R
+import com.example.kubuku.databinding.FragmentLibraryBinding
+
+class LibraryFragment : Fragment() {
+    private lateinit var binding: FragmentLibraryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLibraryBinding.inflate(inflater)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/kubuku/page/DashboardActivity.kt
@@ -0,0 +1,44 @@
+package com.example.kubuku.page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kubuku.R
+import com.example.kubuku.databinding.ActivityDashboardBinding
+import com.example.kubuku.fragment.HomeFragment
+import com.example.kubuku.fragment.LibraryFragment
+import com.example.kubuku.fragment.ProfileFragment
+
+class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            navbarDashboard.selectedItemId = R.id.itemHome
+            replaceFragment(HomeFragment())
+
+            navbarDashboard.setOnItemSelectedListener() {
+                when(it.itemId) {
+                    R.id.itemHome -> replaceFragment(HomeFragment())
+                    R.id.itemLibrary -> replaceFragment(LibraryFragment())
+                    R.id.itemProfile -> replaceFragment(ProfileFragment())
+                    else -> {}
+                }
+                true
+            }
+        }
+    }
+
+    //Function
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+}
