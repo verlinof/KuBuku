25ec326aa26fa8cfdf2e5219942a00a2c2868e87
suparminxd <113745726+verlinof@users.noreply.github.com>
2024-02-17 15:40:30 +0700

    Zuhdi (#14)
    * test
    
    * createNew: <register> design ui
    
    * redesign: <register> change margin between form box
    
    * createNew: <detailBooks> design ui | redesign: <login><register> change some attributes
    
    * createNew: <detailBooks> design ui | redesign: <login><register> change some attributes
    
    * feat: <detailBooks> add a list for other books and a list for bundle package
    
    * createNew: <profile> design ui
    
    * fix: <profile> design ui
    
    * createNew: <detailBooks> design ui | redesign: <login><register> change some attributes
    
    ---------
    
    Co-authored-by: zuhdy-ev <ardhiekov@gmail.com>

---
+++ app/src/main/java/com/example/kubuku/page/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kubuku.page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kubuku.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
