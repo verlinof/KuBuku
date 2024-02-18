a2ecf46ac9cf9363672f925df7509ff87d7e29c4
zuhdy-ev <ardhiekov@gmail.com>
2024-02-18 11:30:24 +0700

    createNew: <paymentFlow> design UI

---
+++ app/src/main/java/com/example/kubuku/TransactionReturn.kt
@@ -0,0 +1,11 @@
+package com.example.kubuku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransactionReturn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction_return)
+    }
+}
